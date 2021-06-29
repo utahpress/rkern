c6214c4cf6c3e896f05e83ea65383f47b8930303
utahpress <86483398+utahpress@users.noreply.github.com>
2021-06-29 13:49:35 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/10Ya7gJPGohwlye-cdMAWexAL8k1CNXXeWTqjQb---H0/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyA48ClWFkBzIgo1BbWgbEX7pPVeh82l9o8';
